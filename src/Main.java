fa00f4df139e98b1e5d3c050f146f0676e052715
theovier <support@testoramus.de>
2016-06-16 17:49:25 +0200

    first successful login

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,12 @@
-import java.io.IOException;
-import java.util.Map;
-
-import org.jsoup.Connection.Method;
-import org.jsoup.Connection.Response;
-import org.jsoup.Jsoup;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args ) throws Exception {
 
-        Response res = Jsoup
-                .connect("https://campusportal.hshl.de")
-                .data("user_name", "user") //replace
-                .data("password", "password") //replace
-                .data("hshl_domain", "@stud.hshl.de")
-                .data("repository", "stud.hshl.de")
-                .data("site_name", "campusportal")
-                .data("secure", "1")
-                .data("resource_id", "2")
-                .data("login_type", "2")
-                .method(Method.POST)
-                .execute();
+        String user = "user"; //Vorname.Nachname
+        String password = "password";
 
-        System.out.println (res.body());
-
-        //This will get you cookies
-        Map<String, String> loginCookies = res.cookies();
+		Client client = new Client(user, password);
+        client.login();
     }
 }
+
