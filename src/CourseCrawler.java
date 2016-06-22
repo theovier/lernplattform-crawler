e2c512768d9a6b7eb9741aa34def072d427a44af
theovier <theo@harkenbusch.com>
2016-06-22 14:30:11 +0200

    refactored

--- src/CourseCrawler.java
+++ src/CourseCrawler.java
@@ -8,18 +8,21 @@ public class CourseCrawler extends Crawler {
     private String semesteryear;
 
     public CourseCrawler(String semester, String year) {
-       semester = semester + " " + year;
+        semesteryear = semester + " " + year;
     }
 
-    //todo refactor
     public List<String> fetchCourseLinks(HtmlPage overviewPage) {
-        List<String> links = new ArrayList<String>();
-        HtmlLabel kurslisteLabel = (HtmlLabel) overviewPage.getFirstByXPath("//label[@title='Sommersemester 2016']");
-        HtmlOrderedList courseList = (HtmlOrderedList) kurslisteLabel.getNextElementSibling().getNextElementSibling();
-        courseList.getElementsByTagName("a").forEach(course -> {
+        List<String> links = new ArrayList<>();
+        fetchCourses(overviewPage).forEach(course -> {
             HtmlAnchor anchor = (HtmlAnchor) course;
             links.add(anchor.getHrefAttribute());
         });
         return links;
+    }
+
+    private List<HtmlElement> fetchCourses(HtmlPage overviewPage) {
+        HtmlLabel courseListLabel = overviewPage.getFirstByXPath("//label[@title='Sommersemester 2016']");
+        HtmlOrderedList courseList = (HtmlOrderedList) courseListLabel.getNextElementSibling().getNextElementSibling();
+        return courseList.getElementsByTagName("a");
     }
 }
