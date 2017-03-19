4e8e006963d6ed60153733ec0a34771cbaa8d9b7
Theo H <support@testoramus.de>
2017-03-19 19:46:23 +0100

    added new graphical indicators for skipped and failed downloads

---
+++ src/main/java/com/lailaps/ui/DownloadIndicator/FailedDownloadIndicator.java
@@ -0,0 +1,12 @@
+package com.lailaps.ui.DownloadIndicator;
+
+
+import javafx.scene.control.Control;
+
+public class FailedDownloadIndicator extends DownloadIndicatorBox {
+
+    @Override
+    public void bindProgressBarWidthProperty(Control parent) {
+
+    }
+}
---
+++ src/main/java/com/lailaps/ui/DownloadIndicator/SkippedDownloadIndicator.java
@@ -0,0 +1,12 @@
+package com.lailaps.ui.DownloadIndicator;
+
+
+import javafx.scene.control.Control;
+
+public class SkippedDownloadIndicator extends DownloadIndicatorBox {
+
+    @Override
+    public void bindProgressBarWidthProperty(Control parent) {
+
+    }
+}
