f9632cfb08d6ffdeeb8befcf562ffad447e33034
Theo H <support@testoramus.de>
2017-03-19 17:16:24 +0100

    added test texts to the progressbars

--- src/main/java/com/lailaps/ui/DownloadScreenController.java
+++ src/main/java/com/lailaps/ui/DownloadScreenController.java
@@ -1,13 +1,17 @@
 package com.lailaps.ui;
 
+import com.lailaps.download.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -24,22 +28,33 @@ public class DownloadScreenController implements Initializable, Controllable, Au
     private MenuBar menubar;
 
     @FXML
-    private ListView<ProgressBar> listView;
+    private ListView<HBox> listView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         determineTimeToBindSizes();
         ObservableList<ProgressBar> bars = FXCollections.observableArrayList();
 
+        ObservableList<HBox> boxes = FXCollections.observableArrayList();
+
         for (int i = 0; i <= 100; i++) {
             ProgressBar a = new ProgressBar(0.01f * i);
             a.setPrefWidth(300);
             bars.add(a);
+
+            Label label = new Label("test");
+            label.setPadding(new Insets(0,0,0,10));
+            HBox hbox = new HBox(a, label);
+            boxes.add(hbox);
         }
-        listView.setItems(bars);
+        listView.setItems(boxes);
         for (ProgressBar bar : bars) {
            // bar.setProgress(1);
         }
+
+
+
+
     }
 
     @Override
