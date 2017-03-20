acbf0dde1c280f399e2dba1ee7d148b8b31cf950
Theo H <support@testoramus.de>
2017-03-20 17:45:45 +0100

    removed duplicated main class

--- src/main/java/com/lailaps/Main.java
+++ src/main/java/com/lailaps/Main.java
@@ -1,11 +1,35 @@
 package com.lailaps;
 
-public class Main {
+import com.lailaps.ui.ScreenContainer;
+import com.lailaps.ui.ScreenType;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-    public static void main(String[] args ) {
 
+public class Main extends Application {
+
+    public static String title = "lailaps";
+
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
     }
-    //todo properties nur aufrufen wenn in admin mode
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ScreenContainer screenContainer = new ScreenContainer();
+        screenContainer.loadAllScreens();
+        screenContainer.showScreen(ScreenType.LOGIN);
+
+        Group root = new Group();
+        root.getChildren().addAll(screenContainer);
+        stage.setScene(new Scene(root));
+        stage.show();
+
+        Image icon = new Image(getClass().getClassLoader().getResourceAsStream("images/lailaps2_32x32.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle(title);
+    }
 }
-
-
