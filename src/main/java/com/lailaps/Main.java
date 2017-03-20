15e916719603497f8225283f7101403810c1a215
Theo H <support@testoramus.de>
2017-03-20 17:41:49 +0100

    removed old GUI implementation

--- src/main/java/com/lailaps/Main.java
+++ src/main/java/com/lailaps/Main.java
@@ -1,12 +1,9 @@
 package com.lailaps;
 
-import com.lailaps.ui.LoginWindow;
-
 public class Main {
 
     public static void main(String[] args ) {
-        LoginWindow window = new LoginWindow();
-        window.show();
+
     }
     //todo properties nur aufrufen wenn in admin mode
 }
