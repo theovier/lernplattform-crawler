3da38310307f5fa63c9af6ab417e020afcd9f1b1
Theo H <support@testoramus.de>
2017-04-15 13:53:09 +0200

    removed unused import

--- src/main/java/com/lailaps/ui/LoginScreenController.java
+++ src/main/java/com/lailaps/ui/LoginScreenController.java
@@ -10,7 +10,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
